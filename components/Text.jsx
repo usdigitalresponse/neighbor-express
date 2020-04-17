9bc6105accfcb6884232c1997704bfefa96f421d
keith kurson <keith@keithkurson.net>
2020-04-16 23:38:52 -0700

    ✨ move /about to CMS

---
+++ components/Text.jsx
@@ -0,0 +1,17 @@
+import Interweave from 'interweave';
+
+const Text = ({ block }) => (<section className="grid-container usa-section" id="share">
+  <div className="grid-row grid-gap">
+    <div className="tablet:grid-col-8 usa-prose">
+      <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+        {block.title}
+      </h2>
+      <p>
+        {block.body}
+      </p>
+    </div>
+  </div>
+</section>
+)
+
+export default Text;
