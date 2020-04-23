6bff290718863a56432a8391377e9bc9efdce34b
Keith Kurson <keith@keithkurson.net>
2020-04-23 09:04:34 -0700

    🎉 Github action to automatically deploy neighbor express
    * Add smart default for languages
    
    * start trying to write git event
    
    * add workflow
    
    * lets try javascripot
    
    * obviously, use require
    
    * 🐛 exec.exec instead of exec(
    
    * 🎉 lets try to deploy the public table to a new url
    
    * add in the zeit key
    
    * move location in update-now.yaml
    
    * 🔥 I hate yaml
    
    * 👀 Lets see if this deploys to concord, NOT PROD
    
    * 👀 Lets see if this deploys to concord, NOT PROD
    
    * See what we have in our env
    
    * 🐛 Need to pass through env variables
    
    * 🙇 Just run on master
    
    * Add paterson and walnutcreek
    
    * Should be the production version when merging to master
    
    * Correct comment
    
    * Changes from k8

---
+++ scripts/update.js
@@ -0,0 +1,13 @@
+const exec = require('@actions/exec');
+
+// What cities do we serve?
+const cities = ['concord', 'walnutcreek', 'paterson'];
+
+cities.forEach(city => {
+  // Lets get our baseid
+  const baseId = process.env[`AIRTABLE_BASE_ID_${city.toUpperCase()}`]
+  // We are going to throw away our .now folder between deploys
+  exec.exec('rm -rf .now');
+  // And we're going to send it out to the correct vercel location
+  exec.exec(`now --env AIRTABLE_BASE_ID=${baseId} --name ${city} --confirm --scope neighborexpress --token ${process.env.ZEIT_TOKEN} --prod`);
+})
