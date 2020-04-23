0b0b7e0904b5d95584480834ae5401939194a044
keith kurson <keith@keithkurson.net>
2020-04-22 19:45:58 -0700

    🎉 lets try to deploy the public table to a new url

--- scripts/update.js
+++ scripts/update.js
@@ -1,3 +1,7 @@
 const exec = require('@actions/exec');
 
-exec.exec('echo testing');
+
+// We are going to throw away our .now folder, and just do some direct deploys
+exec.exec('rm -rf .now');
+// Lets start with paterson
+exec.exec('now --env AIRTABLE_BASE_ID=appZcB8F2bEaLboM0 --name keithtesting --confirm --scope neighborexpress --prod');
