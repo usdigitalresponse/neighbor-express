f8e99c2a8279026676959f823b834bcb0fb92c72
keith kurson <keith@keithkurson.net>
2020-04-23 16:08:56 -0700

    🔍 Remove console.log

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 const Button = ({ href, children, size = 'small' }) => {
-  console.log(href);
   return <p>
     <Link href="/[pid]" as={href}>
       <a className={`usa-button usa-button--${size}`} href={href}>{children}
