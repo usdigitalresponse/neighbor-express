b74e7c15036988bb3aab605008ad7d7412410eeb
keith kurson <keith@keithkurson.net>
2020-04-22 19:51:20 -0700

    add in the zeit key

--- scripts/update.js
+++ scripts/update.js
@@ -4,4 +4,4 @@ const exec = require('@actions/exec');
 // We are going to throw away our .now folder, and just do some direct deploys
 exec.exec('rm -rf .now');
 // Lets start with paterson
-exec.exec('now --env AIRTABLE_BASE_ID=appZcB8F2bEaLboM0 --name keithtesting --confirm --scope neighborexpress --prod');
+exec.exec(`now --env AIRTABLE_BASE_ID=appZcB8F2bEaLboM0 --name keithtesting --confirm --scope neighborexpress --prod --token ${process.env.ZEIT_TOKEN}`);
