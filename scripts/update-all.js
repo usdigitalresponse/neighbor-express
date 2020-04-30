2ec5e235b99dcd29c85b71355a787cee2e335e56
Kate Rudolph <k8reindeer@gmail.com>
2020-04-29 18:43:28 -0700

    uh plz do them one after th other

--- scripts/update-all.js
+++ scripts/update-all.js
@@ -8,7 +8,7 @@ const cities = ['demo2', 'demo3'];
 cities.forEach(city => {
   // Lets get our baseid
   const baseId = process.env[`AIRTABLE_BASE_ID_${city.toUpperCase()}`]
-  populateCmsCache(baseId, process.env['AIRTABLE_API_KEY']);
+  await populateCmsCache(baseId, process.env['AIRTABLE_API_KEY']);
   // We are going to throw away our .now folder between deploys
   exec.exec('rm -rf .now');
   // And we're going to send it out to the correct vercel location
