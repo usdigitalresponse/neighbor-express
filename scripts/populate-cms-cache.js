fa297ff03ceedcb343970c6243fa48bf2c2b0fe6
Kate Rudolph <k8reindeer@gmail.com>
2020-04-29 19:10:51 -0700

    fail gracefully when the creds don't work for whatever reason

--- scripts/populate-cms-cache.js
+++ scripts/populate-cms-cache.js
@@ -4,7 +4,8 @@ const airtable = require('../utils/airtable.js');
 
 async function populateCmsCache (baseId, apiKey) {
 	const cacheResults = await airtable.getCms(baseId, apiKey).catch((err) => {
-		console.log(err)
+		console.log(err);
+		return {"records": []};
 	});
 
 	fs.writeFile("./cms-cache.json", JSON.stringify(cacheResults, null, 4), (err) => {
