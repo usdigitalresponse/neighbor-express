db62bf1297e6f2c6d5004bedddd25d5e98911e74
Keith <keith@keithkurson.net>
2020-06-09 16:40:08 -0700

    🐛 GA tracking only if you have GA_TRACKING_ID secret

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,10 +8,12 @@ export default class MyDocument extends Document {
 			<html>
 				<Head>
 					{/* Global Site Tag (gtag.js) - Google Analytics */}
-					<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
+					{process.env.GA_TRACKING_ID && (
+						<>
+							<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -19,8 +21,10 @@ export default class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
-						}}
-					/>
+								}}
+							/>{' '}
+						</>
+					)}
 				</Head>
 				<body>
 					<Main />
