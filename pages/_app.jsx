c6c5c4722cb3a0f77d8f14b8617de31b8bacf3e0
Keith <keith@keithkurson.net>
2020-06-09 16:20:58 -0700

    ✨ Add google analytics to pages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,83 +1,97 @@
 import React, { useEffect, useState, useContext } from 'react';
-import Router from 'next/router'
+import Router from 'next/router';
 import NeighborLayout from '../layouts/neighbor.jsx';
 import { CMSContextProvider, CMSContext } from '@/context/cms.js';
-import NProgress from 'nprogress'
+import NProgress from 'nprogress';
 import { getCmsRecordFromKey, processRecords } from '@/utils/cms';
 import './styles.css';
 import { NextSeo } from 'next-seo';
+import * as gtag from '../utils/ganalytics';
 
-Router.events.on('routeChangeStart', url => {
-  console.log(`Loading: ${url}`)
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+const handleRouteChange = (url) => {
+	gtag.pageview(url);
+};
+
+Router.events.on('routeChangeStart', (url) => {
+	NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function NeighborExpress({ children }) {
-  let { state, dispatch } = useContext(CMSContext);
+	let { state, dispatch } = useContext(CMSContext);
 
-  useEffect(() => {
-    fetch('/api/get-cms').then((res) => res.json()).then((json) => json.records).then((records) => {
-      dispatch({ type: 'set-records', payload: processRecords(records, state) });
-    });
-  }, []);
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+			NProgress.done();
+		};
+		Router.events.on('routeChangeStart', (url) => {
+			NProgress.start();
+		});
+		Router.events.on('routeChangeComplete', handleRouteChange);
+		Router.events.on('routeChangeError', () => NProgress.done());
+		return () => {
+			Router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, []);
 
-  return <NeighborLayout>
-    {children}
-  </NeighborLayout>
+	useEffect(() => {
+		fetch('/api/get-cms')
+			.then((res) => res.json())
+			.then((json) => json.records)
+			.then((records) => {
+				dispatch({ type: 'set-records', payload: processRecords(records, state) });
+			});
+	}, []);
+
+	return <NeighborLayout>{children}</NeighborLayout>;
 }
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { hasError: false };
+	}
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
+	static getDerivedStateFromError() {
+		return { hasError: true };
+	}
 
-  componentDidCatch(error, errorInfo) {
+	componentDidCatch(error, errorInfo) {}
 
-  }
+	render() {
+		if (this.state.hasError) {
+			return 'Internal Error.';
+		}
 
-  render() {
-    if (this.state.hasError) {
-      return 'Internal Error.';
-    }
-
-    return this.props.children;
-  }
+		return this.props.children;
+	}
 }
 
 function CustomSeo() {
-  let { state, dispatch } = useContext(CMSContext);
-  const title = getCmsRecordFromKey('title', state);
-  const openGraph = title ? {
-    title: title.title,
-    description: title.body
-  } : null;
-  return (title ? <NextSeo
-    title={title.title}
-    description={title.body}
-    openGraph={openGraph}
-  /> : null)
-
+	let { state, dispatch } = useContext(CMSContext);
+	const title = getCmsRecordFromKey('title', state);
+	const openGraph = title
+		? {
+				title: title.title,
+				description: title.body,
+		  }
+		: null;
+	return title ? <NextSeo title={title.title} description={title.body} openGraph={openGraph} /> : null;
 }
 
-
 function App({ Component, pageProps }) {
-  return (
-    <ErrorBoundary>
-      <CMSContextProvider>
-        <CustomSeo />
-        <NeighborExpress>
-          <Component {...pageProps} />
-        </NeighborExpress>
-      </CMSContextProvider>
-    </ErrorBoundary>
-  );
+	return (
+		<ErrorBoundary>
+			<CMSContextProvider>
+				<CustomSeo />
+				<NeighborExpress>
+					<Component {...pageProps} />
+				</NeighborExpress>
+			</CMSContextProvider>
+		</ErrorBoundary>
+	);
 }
 
 // Only uncomment this method if you have blocking data requirements for
