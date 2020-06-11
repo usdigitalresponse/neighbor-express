2f4369779c434d30b8f4ea0de02ccaaaf5f25cf3
Keith Kurson <keith@keithkurson.net>
2020-06-11 10:19:15 -0700

    update cache file when we get a new result from the API (#137)

--- pages/api/get-cms.js
+++ pages/api/get-cms.js
@@ -2,9 +2,12 @@
 * This is the *only* server-side file for neighbor express.
 */
 const airtable = require('../../utils/airtable.js');
+const populateCmsCacheFromJson = require('../../utils/cmsCache.js');
 
 // This takes the data from our CMS airtable
 // And sends it off to the frontend
 export default async (req, res) => {
-  res.send(await airtable.getCms(process.env.AIRTABLE_BASE_ID, process.env.AIRTABLE_API_KEY));
+  const data = await airtable.getCms(process.env.AIRTABLE_BASE_ID, process.env.AIRTABLE_API_KEY);
+  populateCmsCacheFromJson(data);
+  res.send(data);
 };
--- scripts/populate-cms-cache.js
+++ scripts/populate-cms-cache.js
@@ -2,20 +2,20 @@ var fs = require("fs");
 const airtable = require('../utils/airtable.js');
 
 
-async function populateCmsCache (baseId, apiKey) {
+async function populateCmsCache(baseId, apiKey) {
 	const cacheResults = await airtable.getCms(baseId, apiKey).catch((err) => {
 		console.log(err);
-		return {"records": []};
+		return { "records": [] };
 	});
 
 	const fileOutput = `module.exports = ${JSON.stringify(cacheResults, null, 4)}`;
 
 	fs.writeFile("./cms-cache.js", fileOutput, (err) => {
-	  if (err) {
-	    console.error(err);
-	    return;
-	  };
-	  console.log("Cache file populated");
+		if (err) {
+			console.error(err);
+			return;
+		};
+		console.log("Cache file populated");
 	});
 }
 
