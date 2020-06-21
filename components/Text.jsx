cb9a9849b1ee57770d17a7cdc5f41cda8792d7ae
Lien Hoang <kimlienhg@gmail.com>
2020-06-21 00:28:13 -0700

    Have text span across the screen, not just confined to the left

--- components/Text.jsx
+++ components/Text.jsx
@@ -8,7 +8,7 @@ const Text = ({ block }) => (<Content block={block}>
         <img src={block.image} alt={block.alt}/>
       </div>
     }
-    <div className="tablet:grid-col-8 usa-prose">
+    <div className="tablet:grid-col-12 usa-prose">
       <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
         {block.title}
       </h2>
@@ -18,7 +18,7 @@ const Text = ({ block }) => (<Content block={block}>
     </div>
     {
       block.image && block.data?.includes('image:right') &&
-      <div className="tablet:grid-col-4">
+      <div className="tablet:grid-col-12">
         <img src={block.image} alt={block.alt} />
       </div>
     }
