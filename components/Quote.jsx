7cf70b44c7c38e9dc526dafd3df1a69aa3dc4061
Lien Hoang <kimlienhg@gmail.com>
2020-06-23 10:54:05 -0700

    Add carousel to showcase multiple testimonials

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -1,12 +1,23 @@
 import Button from './Button';
+import styles from './Quote.module.css'
+import { Carousel } from 'react-responsive-carousel';
+const classNames = require('classnames');
 
-const Quote = ({ block }) => (
-  <section className="usa-section usa-section--dark">
+const DELIMITER = '@@@';
+
+const Quote = ({ block }) => {
+  const quotes = block.title.split(DELIMITER);
+  
+  return <section className="usa-section usa-section--dark">
     <div className="grid-container">
-      <h2 className="font-heading-xl margin-y-0">{block.title}</h2>
+      <Carousel className={`${styles.carousel}`}>
+        {quotes.map((quote) => {
+          return <h2 className="font-heading-xl margin-y-0">{quote}</h2>
+        })}
+      </Carousel>
       {block.href && <Button href={block.href} size='big'>{block.body}</Button>}
     </div>
   </section>
-)
+}
 
 export default Quote;
