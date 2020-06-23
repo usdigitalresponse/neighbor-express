fa43ce8a64458f46bf718442993d5a03f633a5f9
Lien Hoang <kimlienhg@gmail.com>
2020-06-23 11:15:49 -0700

    Move changes to the Quotes block to another branch

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -1,18 +1,12 @@
 import Button from './Button';
-import styles from './Quote.module.css'
-const classNames = require('classnames');
 
-const Quote = ({ block }) => {
-  const blockClasses =
-    classNames("usa-section", "usa-section--dark", styles.block);
-  return (
-    <section className={blockClasses}>
-      <div className="grid-container">
-        <h2 className="font-heading-xl margin-y-0">{block.title}</h2>
-        {block.href && <Button href={block.href} size='big'>{block.body}</Button>}
-      </div>
-    </section>
-  );
-}
+const Quote = ({ block }) => (
+  <section className="usa-section usa-section--dark">
+    <div className="grid-container">
+      <h2 className="font-heading-xl margin-y-0">{block.title}</h2>
+      {block.href && <Button href={block.href} size='big'>{block.body}</Button>}
+    </div>
+  </section>
+)
 
 export default Quote;
