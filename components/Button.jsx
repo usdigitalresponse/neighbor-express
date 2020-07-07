a78e59fc8cc5e9f24e52b46a7450a37b53673f2b
Lien Hoang <alienngator@users.noreply.github.com>
2020-07-07 00:00:56 -0700

    Miscellaneous small layout fixes (bug #143) (#154)
    * Update quote text color from blue to white
    
    * Update quote text color from blue to white
    
    * Move the language switcher to the top of the page
    
    * Move simple styling changes to another branch
    
    * Revert "Move simple styling changes to another branch"
    
    This reverts commit f0e7bc060a4e7f17c6a9ba5bb47d769bfc83f93c.
    
    * Change quotes color from blue to white
    
    * Have text span across the screen, not just confined to the left
    
    * Allow button to route to absolute links
    
    * Move changes to the Quotes block to another branch
    
    * Make embedded Youtube videos smaller
    
    * Undo irrelevant changes

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,12 +1,21 @@
 import Link from 'next/link';
 
 const Button = ({ href, children, size = 'small' }) => {
-  return <p>
-    <Link href="/[pid]" as={href}>
-      <a className={`usa-button usa-button--${size}`} href={href}>{children}
+  const isRelativePath = href.trimLeft().startsWith('/');
+  if (isRelativePath) {
+    return <p>
+      <Link href="/[pid]" as={href}>
+        <a className={`usa-button usa-button--${size}`} href={href}>{children}
+        </a>
+      </Link>
+    </p>
+  } else {
+    return <p>
+      <a className={`usa-button usa-button--${size}`} href={href}>
+        {children}
       </a>
-    </Link>
-  </p>
+    </p>
+  } 
 }
 
 export default Button;
